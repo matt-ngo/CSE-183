8cf8d1792af1a471a53e97d6a03b1926048b8cbb
Matt Ngo <matthewngo98@gmail.com>
2020-10-23 16:49:11 -0700

    renamed dirs

---
+++ Assignment4-JavaScriptII/Assignment4/Templater.js
@@ -0,0 +1,22 @@
+/**
+ * CSE183 Assignment 4 - Basic
+ */
+class Templater {
+  /**
+   * Replace the contents of {{ }} tagged table header and data 
+   * elements in document with values found in the supplied JSON
+   * @param {object} document 
+   * @param {string} json with propeties matching tags in document
+   */
+  byTag(document, json) {
+  }
+
+  /**
+   * Replace the contents of table header and data elements in
+   * in document with id'd content found in the supplied JSON
+   * @param {object} document 
+   * @param {string} json with propeties matching element ids in document
+   */
+  byId(document, json) {
+  }
+}
